6a61a35b0b070e53511d9d64519a759dc9f0bd30
Aaron Walker <a.walker@base2services.com>
2022-03-02 13:38:56 +0000

    tag fixes

--- transit-gateway-vpc-attachment.cfndsl.rb
+++ transit-gateway-vpc-attachment.cfndsl.rb
@@ -1,19 +1,24 @@
 CloudFormation do
 
-    default_tags = external_parameters.fetch(:default_tags, [])
-  
-    EC2_TransitGatewayAttachment(:NetworkVpcGatewayAttachment) do
-      TransitGatewayId Ref(:TransitGatewayId)
-      VpcId Ref(:VPCId)
-      SubnetIds Ref(:SubnetIds)
-      Tags default_tags
-    end
+  default_tags = external_parameters.fetch(:default_tags, {})
 
-    EC2_TransitGatewayRoute(:VpcGatewayRoute) do
-      Blackhole 'false'
-      DestinationCidrBlock Ref(:VpcCidr)
-      TransitGatewayAttachmentId Ref(:NetworkVpcGatewayAttachment)
-      TransitGatewayRouteTableId Ref(:TransitGatewayRouteTableId)
-    end
+  tags = []
+  default_tags.each do |key, value|
+    tags << {Key: key, Value: value}
+  end unless default_tags.nil?
+  
+  EC2_TransitGatewayAttachment(:NetworkVpcGatewayAttachment) do
+    TransitGatewayId Ref(:TransitGatewayId)
+    VpcId Ref(:VPCId)
+    SubnetIds Ref(:SubnetIds)
+    Tags tags
+  end
+
+  EC2_TransitGatewayRoute(:VpcGatewayRoute) do
+    Blackhole 'false'
+    DestinationCidrBlock Ref(:VpcCidr)
+    TransitGatewayAttachmentId Ref(:NetworkVpcGatewayAttachment)
+    TransitGatewayRouteTableId Ref(:TransitGatewayRouteTableId)
+  end
   
   end
